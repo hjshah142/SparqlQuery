9586ecc4d35f85158df257c12e5682c0a07cd4cf
Harsh Shah <harsh@hjshah>
2018-10-26 20:53:33 +0200

    reading file added

--- src/RDF_read.java
+++ src/RDF_read.java
@@ -1,9 +1,35 @@
 
-public class RDF_read {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.util.FileManager;
 
-	}
+import java.io.*;
 
+/** Tutorial 5 - read RDF XML from a file and write it to standard out
+ */
+public class RDF_read extends Object {
+
+    /**
+        NOTE that the file is loaded from the class-path and so requires that
+        the data-directory, as well as the directory containing the compiled
+        class, must be added to the class-path when running this and
+        subsequent examples.
+    */    
+    static final String inputFileName  = "vc-db-1.rdf";
+                              
+    public static void main (String args[]) {
+        // create an empty model
+        Model model = ModelFactory.createDefaultModel();
+
+        InputStream in = FileManager.get().open( inputFileName );
+        if (in == null) {
+            throw new IllegalArgumentException( "File: " + inputFileName + " not found");
+        }
+        
+        // read the RDF/XML file
+        model.read(in, "");
+                    
+        // write it to standard out
+        model.write(System.out);            
+    }
 }
